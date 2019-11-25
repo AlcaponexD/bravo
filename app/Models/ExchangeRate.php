2f4afd3ca485b6fadb13fc6e8c004ae84c9a2853
jeison Pedroso <jeison.contas@gmail.com>
2019-11-25 20:33:10 -0300

    fix campos fiilable

--- app/Models/ExchangeRate.php
+++ app/Models/ExchangeRate.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class EnchangeRate extends Model
+class ExchangeRate extends Model
 {
     protected $table = 'exchange_rates';
 
     protected $fillable = [
         'to',
         'from',
+        'user_id',
+        'value'
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
         'user_id'
     ];
 }
